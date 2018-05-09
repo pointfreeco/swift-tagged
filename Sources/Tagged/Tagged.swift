a980af3c85dfbb1ac1970a371ffbcf542801c69c
Brandon Williams <mbw234@gmail.com>
2018-05-08 22:36:08 -0400

    Update Tagged.swift

--- Sources/Tagged/Tagged.swift
+++ Sources/Tagged/Tagged.swift
@@ -78,12 +78,6 @@ extension Tagged: ExpressibleByIntegerLiteral where RawValue: ExpressibleByInteg
   }
 }
 
-extension Tagged: ExpressibleByNilLiteral where RawValue: ExpressibleByNilLiteral {
-  public init(nilLiteral: ()) {
-    self.init(rawValue: RawValue(nilLiteral: nilLiteral))
-  }
-}
-
 extension Tagged: ExpressibleByStringLiteral where RawValue: ExpressibleByStringLiteral {
   public typealias StringLiteralType = RawValue.StringLiteralType
 
