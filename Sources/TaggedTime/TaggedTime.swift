257eed1dc8b92139732328a1be2a9b37e9d5e121
Brandon Williams <mbw234@gmail.com>
2018-07-10 10:02:08 -0400

    date conversions and more tests

--- Sources/TaggedTime/TaggedTime.swift
+++ Sources/TaggedTime/TaggedTime.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Tagged
 
 public enum MillisecondsTag {}
@@ -6,14 +7,35 @@ public typealias Milliseconds<A> = Tagged<MillisecondsTag, A>
 public enum SecondsTag {}
 public typealias Seconds<A> = Tagged<SecondsTag, A>
 
+extension Tagged where Tag == MillisecondsTag {
+
+}
+
 extension Tagged where Tag == MillisecondsTag, RawValue: BinaryFloatingPoint {
   public var seconds: Seconds<RawValue> {
     return .init(rawValue: self.rawValue / 1000)
+  }
+
+  public var date: Date {
+    let seconds = self.seconds
+    let double = Double(
+      sign: seconds.rawValue.sign,
+      exponentBitPattern: UInt(seconds.rawValue.exponentBitPattern),
+      significandBitPattern: UInt64(seconds.rawValue.significandBitPattern)
+    )
+
+    return Date(timeIntervalSince1970: double)
   }
 }
 
 extension Tagged where Tag == SecondsTag, RawValue: Numeric {
   public var milliseconds: Milliseconds<RawValue> {
     return .init(rawValue: self.rawValue * 1000)
+  }
+}
+
+extension Tagged where Tag == SecondsTag, RawValue: BinaryInteger {
+  public var date: Date {
+    return Date(timeIntervalSince1970: Double(Int64(self.rawValue)))
   }
 }
--- Tests/TaggedTimeTests/TaggedTimeTests.swift
+++ Tests/TaggedTimeTests/TaggedTimeTests.swift
@@ -11,4 +11,18 @@ final class TaggedTimeTests: XCTestCase {
     let milliseconds: Milliseconds<Double> = 12000
     XCTAssertEqual(12.0, milliseconds.seconds)
   }
+
+  func testDate() {
+    let seconds: Seconds<Int> = 12
+    XCTAssertEqual(Date(timeIntervalSince1970: 12), seconds.date)
+
+    let milliseconds: Milliseconds<Double> = 12000
+    XCTAssertEqual(Date(timeIntervalSince1970: 12), milliseconds.date)
+  }
+
+  func testLossyMillisecondsToSeconds() {
+    let milliseconds: Milliseconds<Int> = 12500
+    let seconds = milliseconds.map(Double.init).seconds.map(Int.init)
+    XCTAssertEqual(12, seconds)
+  }
 }
