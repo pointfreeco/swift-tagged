278e89b25e44b663a9ff238e0e488132b0f4aa81
Brandon Williams <mbw234@gmail.com>
2018-07-10 10:07:50 -0400

    clean up

--- Sources/TaggedTime/TaggedTime.swift
+++ Sources/TaggedTime/TaggedTime.swift
@@ -7,14 +7,13 @@ public typealias Milliseconds<A> = Tagged<MillisecondsTag, A>
 public enum SecondsTag {}
 public typealias Seconds<A> = Tagged<SecondsTag, A>
 
-extension Tagged where Tag == MillisecondsTag {
-
-}
-
 extension Tagged where Tag == MillisecondsTag, RawValue: BinaryFloatingPoint {
   public var seconds: Seconds<RawValue> {
     return .init(rawValue: self.rawValue / 1000)
   }
+}
+
+extension Tagged where Tag == MillisecondsTag, RawValue: BinaryFloatingPoint {
 
   public var date: Date {
     let seconds = self.seconds
