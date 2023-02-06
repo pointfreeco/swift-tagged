d92e8006b7369d195e8f7933cb94d78ae9c018fe
Stephen Celis <stephen@stephencelis.com>
2023-02-06 08:16:01 -0800

    Update UUID.swift

--- Sources/Tagged/UUID.swift
+++ Sources/Tagged/UUID.swift
@@ -11,7 +11,7 @@ extension Tagged where RawValue == UUID {
   /// Creates a tagged UUID from a string representation.
   ///
   /// - Parameter string: The string representation of a UUID, such as
-  ///   `DEADBEEF-DEAD-BEED-DEAD-BEEFDEADBEEF`.
+  ///   `DEADBEEF-DEAD-BEEF-DEAD-BEEFDEADBEEF`.
   public init?(uuidString string: String) {
     guard let uuid = UUID(uuidString: string)
     else { return nil }
