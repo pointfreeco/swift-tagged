416c4140901dbd1ac18243497d672630d993b0da
David Roman <2538074+davdroman@users.noreply.github.com>
2025-07-18 14:34:39 +0100

    WIP

--- Tests/TaggedMacroPluginTests/TaggedMacroTests.swift
+++ Tests/TaggedMacroPluginTests/TaggedMacroTests.swift
@@ -6,9 +6,20 @@ struct TaggedMacroTests {
   @Test
   func testTaggedInt() {
     struct User {
-      #Tagged<String>("ID")
-//      #TaggedTypealias("ID", as: UUID.self)
-//      #Tagged("ID", as: UUID.self)
+      #Tagged<Int>("ID")
+      #Tagged<Int>("Age")
+
+      let id: ID
+      let age: Age
+
+      init(id: ID, age: Age) {
+        self.id = id
+        self.age = age
+      }
     }
+    let user = User(id: 1, age: 42)
+    #expect(User.ID.self != User.Age.self)
+    #expect(user.id == 1)
+    #expect(user.age == 42)
   }
 }
