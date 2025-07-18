064c39ed5ebeacf1741512abe83b0777cbcaf2b6
David Roman <2538074+davdroman@users.noreply.github.com>
2025-07-18 10:24:10 +0100

    WIP

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -20,5 +20,5 @@ var package = Package(
     .target(name: "TaggedTime", dependencies: ["Tagged"]),
     .testTarget(name: "TaggedTimeTests", dependencies: ["TaggedTime"]),
   ],
-  swiftLanguageVersions: [.v6]
+  swiftLanguageModes: [.v6]
 )
